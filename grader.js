f2a2e1d25cc5aa71cbb31d952e66d5de7724f061
hessions <hessions@tcd.ie>
2013-08-01 09:39:13 +0000

    updated grader.js

--- grader.js
+++ grader.js
@@ -26,6 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://salty-beach-4766.herokuapp.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -64,7 +65,8 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url ', 'Path to http://salty-beach-4766.herokuapp.com', URL_DEFAULT)
+        .option('-f, --file ', 'index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
